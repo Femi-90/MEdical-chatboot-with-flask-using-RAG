e900e4e922c9d02ba89a5fc9903f9225a4793ef9
Oluwole Omololu <olafemilolu@gmail.com>
2026-05-12 14:40:06 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directory structure
+
+mkdir -p src 
+mkdir -p research
+
+#  Creating files
+touch src/__init__.py
+touch research/__init__.py
+touch  src/prompt.py
+touch .env
+touch  README.md
+touch  app.py
+touch   setup.py
+touch   research/trials.ipynb
+touch  requirements.txt
+
+echo "Directory and files created successfully."
